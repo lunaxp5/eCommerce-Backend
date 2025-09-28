557aedfe9209bea59934d5fcae8f03b737d89e2a
Jose Luna <lunaxp5@gmail.com>
2025-09-27 23:03:09 -0400

    feat: show products with quantity is greater than 0

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -19,7 +19,10 @@ const addProduct = catchAsyncError(async (req, res, next) => {
 });
 
 const getAllProducts = catchAsyncError(async (req, res, next) => {
-  let apiFeature = new ApiFeatures(productModel.find(), req.query)
+  let apiFeature = new ApiFeatures(
+    productModel.find({ quantity: { $gt: 0 } }),
+    req.query
+  )
     .pagination()
     .fields()
     .filteration()
